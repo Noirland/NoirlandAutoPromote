1df71ec320536e36f9947e0f8f49b3baf7c2fc9b
Brynley McDonald <brynleymcdonald@gmail.com>
2013-06-06 15:24:03 +1200

    Added /agree New config option, under a rank: "ranks.[rank].default" Takes 'default' rank and promotes with its 'promoteTo'

--- src/java/me/ZephireNZ/NoirlandAutoPromote/commands/CommandAgree.java
+++ src/java/me/ZephireNZ/NoirlandAutoPromote/commands/CommandAgree.java
@@ -1,7 +1,9 @@
 package me.ZephireNZ.NoirlandAutoPromote.commands;
 
 import me.ZephireNZ.NoirlandAutoPromote.NoirlandAutoPromote;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandAgree extends Command {
     public CommandAgree(NoirlandAutoPromote plugin) {
@@ -10,7 +12,20 @@ public class CommandAgree extends Command {
 
     @Override
     public boolean onCommand(CommandSender sender, org.bukkit.command.Command command, String label, String[] args) {
-
-        return true;
+        if(sender instanceof Player) {
+            Player player = (Player) sender;
+            String currRank = gmHandler.getGroup(player);
+            String rankDefault = confHandler.getDefault();
+            if(currRank.equalsIgnoreCase(rankDefault)) {
+                String newRank = confHandler.getPromoteTo(rankDefault);
+                pmHandler.promote(player, newRank);
+                return true;
+            }else{
+                plugin.sendMessage(sender, ChatColor.RED + "[NoirPromote] " + ChatColor.RESET + "You cannot be promoted, you are not " + confHandler.getDefault());
+                return false;
+            }
+        }
+        plugin.sendMessage(sender, "The console cannot be promoted");
+        return false;
     }
 }

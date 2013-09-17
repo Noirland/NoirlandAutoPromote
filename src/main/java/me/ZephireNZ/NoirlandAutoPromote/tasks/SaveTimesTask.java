faee65861acae8479282ee0d80b21035a8b27f12
Brynley McDonald <brynleymcdonald@gmail.com>
2013-09-17 17:05:09 +1200

    Simplify for loops

--- src/main/java/me/ZephireNZ/NoirlandAutoPromote/tasks/SaveTimesTask.java
+++ src/main/java/me/ZephireNZ/NoirlandAutoPromote/tasks/SaveTimesTask.java
@@ -15,7 +15,11 @@ public class SaveTimesTask extends BukkitRunnable {
 	public void run() {
 		for(Player player : plugin.getServer().getOnlinePlayers()) { // Check for promoteable player
 			plugin.pmHandler.checkForPromotion(player);
-		}
+            //TODO: Add running of task with time to promotion less than the saveTime's interval
+
+        }
+
+
         plugin.saveToDB(true); // Save all players to DB on event
 	}
 }

29bddca9bb4253f859edee11e88da7488732577f
ZephireNZ <brynleymcdonald@gmail.com>
2014-06-08 14:23:41 +1200

    And again, to add missing comma

--- src/main/java/nz/co/noirland/noirlandautopromote/database/Queries.java
+++ src/main/java/nz/co/noirland/noirlandautopromote/database/Queries.java
@@ -5,6 +5,6 @@ public class Queries {
     public static final String GET_SCHEMA = "SELECT `version` FROM `" + DatabaseTables.SCHEMA.toString() + "`";
 
     public static final String GET_ALL_TIMES = "SELECT * FROM `" + DatabaseTables.TIMES.toString() + "`";
-    public static final String UPDATE_PLAY_TIMES = "INSERT INTO `" + DatabaseTables.TIMES.toString() + "` VALUES(?,?,?) ON DUPLICATE KEY UPDATE `playTime`=VALUES(`playTime`) `totalPlayTime`=VALUES(`totalPlayTime`)";
+    public static final String UPDATE_PLAY_TIMES = "INSERT INTO `" + DatabaseTables.TIMES.toString() + "` VALUES(?,?,?) ON DUPLICATE KEY UPDATE `playTime`=VALUES(`playTime`), `totalPlayTime`=VALUES(`totalPlayTime`)";
 
 }

aac7de797da0637d89ff20140c59e9cff7343598
ZephireNZ <brynleymcdonald@gmail.com>
2014-07-10 14:15:29 +1200

    Also, sort the play times when getting them from the database

--- src/main/java/nz/co/noirland/noirlandautopromote/database/Queries.java
+++ src/main/java/nz/co/noirland/noirlandautopromote/database/Queries.java
@@ -4,7 +4,7 @@ public class Queries {
 
     public static final String GET_SCHEMA = "SELECT `version` FROM `" + DatabaseTables.SCHEMA.toString() + "`";
 
-    public static final String GET_ALL_TIMES = "SELECT * FROM `" + DatabaseTables.TIMES.toString() + "`";
+    public static final String GET_ALL_TIMES = "SELECT * FROM `" + DatabaseTables.TIMES.toString() + "` ORDER BY `totalPlayTime` DESC";
     public static final String UPDATE_PLAY_TIMES = "INSERT INTO `" + DatabaseTables.TIMES.toString() + "` VALUES(?,?,?) ON DUPLICATE KEY UPDATE `playTime`=VALUES(`playTime`), `totalPlayTime`=VALUES(`totalPlayTime`)";
 
 }
